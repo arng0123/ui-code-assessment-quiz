ad9dbd1c32213fcd893a2675395051a6f36e7cd0
Ariel Ng <ng.ariel.elizabeth@gmail.com>
2020-10-07 10:52:40 -0400

    Add: Component Skeleton

---
+++ src/client/Questions.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+function Questions () {
+
+  return(
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+       <h1>Here is where the questions will be rendered</h1>
+    </div>
+  )
+
+};
+
+export default Questions
---
+++ src/client/Summary.js
@@ -0,0 +1,5 @@
+// Number of correct answers
+// Number of incorrect answers
+// Total number of questions answered
+// The final score (%)
+// A button to restart the Quiz with a different questions
